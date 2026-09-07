b8fbac93c11ef4f8c0e8628199bd938175158807
jnunez1179 <joenunez1179@gmail.com>
2026-09-07 11:10:57 -0700

    style(main): Local variable placement
    Moved local variables closer to where they are enacted on.

--- binread/src/main.c
+++ binread/src/main.c
@@ -6,7 +6,6 @@
 #include "../inc/interpret.h"
 #include "../inc/user_input.h"
 
-
 #define NAME_SIZE 20
 #define N_BYTES 4
 #define N_TYPES 9
@@ -17,9 +16,6 @@ int main(int argc, char *argv[])
   Bytes selected_memory;
 
   FILE *file;
-
-  uint32_t offset_one, offset_two;
-
   file = fopen(argv[1], "r");
 
   input = init_buffer();
@@ -30,6 +26,7 @@ int main(int argc, char *argv[])
 
   print_buffer(input, N_BYTES);
 
+  uint32_t offset_one, offset_two;
   printf("\nEnter the first offset of the Range (Range <= 4 bytes): \n");
   scanf("%8X", &offset_one);
   printf("Enter the second offset of the Range (Range <= 4 bytes): \n");
@@ -40,15 +37,11 @@ int main(int argc, char *argv[])
   selected_memory = select_memory(input, offset_one, offset_two);
 
   printf("Range of Bytes: \n");
-
   print_selected_bytes(selected_memory);
 
-  bool machine_endianness;
-
-  machine_endianness = test_endianness();
+  bool machine_endianness = test_endianness();
 
   uint8_t input_endianess;
-
   if ((return_memory_size(selected_memory)) > 1)
   {
     printf("What endianness will the value use? (big/little)\n");
@@ -63,7 +56,6 @@ int main(int argc, char *argv[])
       printf("Please enter \"big\" or \"little\"\n");
     }
   }
-
   printf("Endianness Output: %hhu\n", input_endianess);
 }
 
